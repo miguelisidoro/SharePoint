3d4640b93dcedf8e66f2aee0dfc4dd5c661839b5
miguelisidoro <mlisidoro@gmail.com>
2019-04-06 02:02:40 +0100

    Delete Export-WSP.ps1

--- PowerShell/SharePoint 2019/Export-WSP.ps1
+++
@@ -1,9 +0,0 @@
-$dirName = Get-Location
-
-foreach ($solution in Get-SPSolution)
-{
-    $id = $Solution.SolutionID
-    $title = $Solution.Name
-    $filename = $Solution.SolutionFile.Name
-    $solution.SolutionFile.SaveAs("$dirName\$filename")
-}
