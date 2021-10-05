c5a5768f02abd6e95651aa63512dcf0847a1e51b
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2021-10-05 19:00:18 +0100

    Added new props.

--- SPFx/hello-world/src/webparts/helloWorld/HelloWorldWebPart.ts
+++ SPFx/hello-world/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -1,7 +1,10 @@
 import { Version } from '@microsoft/sp-core-library';
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
+  PropertyPaneTextField,
+  PropertyPaneCheckbox,
+  PropertyPaneDropdown,
+  PropertyPaneToggle
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { escape } from '@microsoft/sp-lodash-subset';
@@ -11,6 +14,10 @@ import * as strings from 'HelloWorldWebPartStrings';
 
 export interface IHelloWorldWebPartProps {
   description: string;
+  test: string;
+  test1: boolean;
+  test2: string;
+  test3: boolean;
 }
 
 export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
@@ -50,6 +57,27 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
               groupFields: [
                 PropertyPaneTextField('description', {
                   label: strings.DescriptionFieldLabel
+                }),
+                PropertyPaneTextField('test', {
+                  label: 'Multi-line text field',
+                  multiline: true
+                }),
+                PropertyPaneCheckbox('test1', {
+                  text: 'Checkbox'
+                }),
+                PropertyPaneDropdown('test2', {
+                  label: 'Dropdown',
+                  options: [
+                    {key: '1', text: 'One'},
+                    {key: '2', text: 'Two'},
+                    {key: '3', text: 'Three'},
+                    {key: '4', text: 'Four'}
+                  ]
+                }),
+                PropertyPaneToggle('test3', {
+                  label: 'Toggle',
+                  onText: 'On',
+                  offText: 'Off'
                 })
               ]
             }
