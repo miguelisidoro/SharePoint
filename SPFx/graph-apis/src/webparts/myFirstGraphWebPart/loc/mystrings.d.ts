9289c41365dad18809f19e936c21211f0ef4ec32
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2021-10-27 15:22:36 +0100

    1st commit from graph api web part

---
+++ SPFx/graph-apis/src/webparts/myFirstGraphWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMyFirstGraphWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MyFirstGraphWebPartWebPartStrings' {
+  const strings: IMyFirstGraphWebPartWebPartStrings;
+  export = strings;
+}
