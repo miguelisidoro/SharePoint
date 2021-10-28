3c8b55bc17029e0768042ba4a22a69db4cfe3de4
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2021-10-28 10:15:21 +0100

    React document card web part finished

--- SPFx/documentcardexample-webpart/src/webparts/documentCardExample/components/DocumentCardExample.tsx
+++ SPFx/documentcardexample-webpart/src/webparts/documentCardExample/components/DocumentCardExample.tsx
@@ -3,23 +3,41 @@ import styles from './DocumentCardExample.module.scss';
 import { IDocumentCardExampleProps } from './IDocumentCardExampleProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
+import {
+  DocumentCard,
+  DocumentCardPreview,
+  DocumentCardTitle,
+  DocumentCardActivity,
+  IDocumentCardPreviewProps
+} from 'office-ui-fabric-react/lib/DocumentCard';
+
 export default class DocumentCardExample extends React.Component<IDocumentCardExampleProps, {}> {
-  public render(): React.ReactElement<IDocumentCardExampleProps> {
+  public render(): JSX.Element {
+    const previewProps: IDocumentCardPreviewProps = {
+      previewImages: [
+        {
+          previewImageSrc: String(require('./document-preview.png')),
+          iconSrc: String(require('./icon-ppt.png')),
+          width: 318,
+          height: 196,
+          accentColor: '#ce4b1f'
+        }
+      ],
+    };
+  
     return (
-      <div className={ styles.documentCardExample }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <DocumentCard onClickHref='http://bing.com'>
+        <DocumentCardPreview { ...previewProps } />
+        <DocumentCardTitle title='Revenue stream proposal fiscal year 2016 version02.pptx' />
+        <DocumentCardActivity
+          activity='Created Feb 23, 2016'
+          people={
+            [
+              { name: 'Kat Larrson', profileImageSrc: String(require('./avatar-kat.png')) }
+            ]
+          }
+        />
+      </DocumentCard>
     );
   }
 }
