cc402c94a4c896ee929f7cbced022fa88542e2aa
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-02-06 23:39:14 +0000

    Added more results web part

---
+++ SPFx/react-birthdays_workanniversaries_newcollaborators/src/webparts/birthdaysWorkAnniversariesNewCollaboratorsMoreResults/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IBirthdaysWorkAnniversariesNewCollaboratorsMoreResultsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'BirthdaysWorkAnniversariesNewCollaboratorsMoreResultsWebPartStrings' {
+  const strings: IBirthdaysWorkAnniversariesNewCollaboratorsMoreResultsWebPartStrings;
+  export = strings;
+}
