180e0e32802e308e39c6ec0ddd43759098895ada
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-02-06 19:41:19 +0000

    Minor changes

--- SPFx/react-birthdays_workanniversaries_newcollaborators/src/webparts/birthdaysWorkAnniversariesNewCollaborators/BirthdaysWorkAnniversariesNewCollaboratorsWebPart.ts
+++ SPFx/react-birthdays_workanniversaries_newcollaborators/src/webparts/birthdaysWorkAnniversariesNewCollaborators/BirthdaysWorkAnniversariesNewCollaboratorsWebPart.ts
@@ -77,7 +77,7 @@ export default class BirthdaysWorkAnniversariesNewCollaboratorsWebPart extends B
                   label: strings.NumberOfItemsToShowLabel,
                   description: strings.NumberOfItemsToShowLabel,
                   value: this.properties.numberOfItemsToShow,
-                  maxValue: 1000,
+                  maxValue: 20,
                   minValue: 1,
                   disabled: false
                 }),
