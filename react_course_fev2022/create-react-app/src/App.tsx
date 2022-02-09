cfeeb30912290038eee6c439b1555e1174c95a9c
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-02-09 20:10:31 +0000

    Continued course

--- react_course_fev2022/create-react-app/src/App.tsx
+++ react_course_fev2022/create-react-app/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import Component1 from './components/component1';
-import Component2 from './components/component2';
+import { Content } from './layout/Content';
+import Footer from './layout/Footer';
+import Header from './layout/Header';
+
 class App extends React.Component {
   render(){
     return (
       <>
-        <Component1 />
-        <Component1 />
-        <Component2 />
+        <Header />
+        <Content />
+        <Footer />
       </>
     )
   }
---
+++ react_course_fev2022/create-react-app/src/components/component3/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Component3.module.css"
+
+export default class Component3 extends React.Component {
+    render() {
+        return <h1 className={styles.header}>Hello world 3</h1>;
+      }
+}
