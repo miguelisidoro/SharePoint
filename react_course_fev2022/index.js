b5ea43540cbcfb038a1de3fba90c2bd4ab0ee290
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-02-09 16:04:13 +0000

    Continued course

--- react_course_fev2022/index.js
+++ react_course_fev2022/index.js
@@ -5,3 +5,29 @@ console.log("Hello world!");
 const circlePerimeter = r => 2*3.14*r
 
 console.log ("circlePerimeter(4)", circlePerimeter(4));
+
+const array1 = ['Apple', 'Orange', 'Banana'];
+
+//clone => faz copia para outra posição para memoria
+const cloneArray = [...array1]
+cloneArray.push("Peaches");
+
+console.log ("cloneArray:", cloneArray);
+
+const marco = {
+    "name": "Marco",
+    "tech": ["Python","React","NodeJs"]
+}
+
+//sread operator -> n faz clone, usa a mesma operação de memoria
+const pedro = {
+    ...marco,
+    name: "Pedro"
+}
+
+console.log("marco = ", marco);
+console.log("pedro = ", pedro);
+
+pedro.tech = [...marco.tech, "Flutter"];
+
+console.log("pedro = ", pedro);
