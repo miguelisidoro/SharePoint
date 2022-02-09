a172be847108266381fa3ff7a4196381f86cdb95
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-02-09 17:07:12 +0000

    Continued course

--- react_course_fev2022/index.js
+++ react_course_fev2022/index.js
@@ -2,9 +2,9 @@ console.log("Hello world!");
 
 //const circlePerimeter = (r) => {return 2*3.14*r}
 //const circlePerimeter = r => {return 2*3.14*r}
-const circlePerimeter = r => 2*3.14*r
+const circlePerimeter = r => 2 * 3.14 * r
 
-console.log ("circlePerimeter(4)", circlePerimeter(4));
+console.log("circlePerimeter(4)", circlePerimeter(4));
 
 const array1 = ['Apple', 'Orange', 'Banana'];
 
@@ -12,11 +12,11 @@ const array1 = ['Apple', 'Orange', 'Banana'];
 const cloneArray = [...array1]
 cloneArray.push("Peaches");
 
-console.log ("cloneArray:", cloneArray);
+console.log("cloneArray:", cloneArray);
 
 const marco = {
     "name": "Marco",
-    "tech": ["Python","React","NodeJs"]
+    "tech": ["Python", "React", "NodeJs"]
 }
 
 //sread operator -> n faz clone, usa a mesma operação de memoria
@@ -31,3 +31,42 @@ console.log("pedro = ", pedro);
 pedro.tech = [...marco.tech, "Flutter"];
 
 console.log("pedro = ", pedro);
+
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const result = circlePerimeter(2)
+        resolve(result);
+    }, 3000);
+});
+
+p.then((result) => { console.log("result = ", result) });
+
+const circleArraySync = (r) => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve(circlePerimeter(r)) }, 3000);
+})
+
+const calculateArea = async () => {
+    try {
+        const result = await circleArraySync(10);
+        console.log("result = ", result);
+    }
+    catch (e) {
+        console.log("error=", e);
+    }
+}
+
+const calculateAreaMultipleCalls = async () => {
+    try {
+        const promises = [circleArraySync(1), circleArraySync(2), circleArraySync(3), circleArraySync(4)]
+
+        const results = await Promise.all(promises)
+        
+        console.log("results = ", results)
+    }
+    catch (e) {
+        console.log("error=", e);
+    }
+}
+
+calculateArea();
+calculateAreaMultipleCalls();
