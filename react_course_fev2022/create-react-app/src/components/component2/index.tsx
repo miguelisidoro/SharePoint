ad128410f85a18448b4e458651aa69d6f48b362d
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-02-09 18:36:45 +0000

    Continued course

--- react_course_fev2022/create-react-app/src/components/component2/index.tsx
+++ react_course_fev2022/create-react-app/src/components/component2/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import style from "./style";
 
 export default class Component2 extends React.Component {
     render() {
-        return <h1>Hello world 2</h1>;
+        return <h1 style={style}>Hello world 2</h1>;
       }
 }
---
+++ react_course_fev2022/create-react-app/src/components/component2/style.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const style: React.CSSProperties = {
+    backgroundColor: "green",
+    fontSize:60
+}
+
+export default style;
