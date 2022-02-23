dd2f15bf21d9c6a8aa006a3b6de5932bea8447e7
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-02-23 16:45:48 +0000

    Continued course

--- react_course_fev2022/create-react-app/src/layout/AppRoutes.tsx
+++ react_course_fev2022/create-react-app/src/layout/AppRoutes.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom"
-import { Component8, Component9 } from '../components';
+import { Component4, Component8, Component9 } from '../components';
+import Component1 from '../components/component1';
+import Component2 from '../components/component2';
+import Component3 from '../components/component3';
 import { NTFCollectionsListPage } from '../containers/NTFCollectionsListPage';
+
+export const PrivateRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Component1 />} />
+            <Route path="/" element={<Component2 />} />
+        </Routes>
+    )
+}
+
+export const PublicRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Component3 />} />
+            <Route path="/" element={<Component4 message="Hello!" />} />
+        </Routes>
+    )
+}
 
 export const AppRoutes = () => {
     return (
@@ -9,6 +30,9 @@ export const AppRoutes = () => {
             <Route path="/component8/:defaultMessage" element={<Component8 message="Default message" />} />
             <Route path="/component9" element={<Component9 message="Default message" />} />
             <Route path="/" element={<NTFCollectionsListPage />} />
+            <Route path="/private" element={<PrivateRoutes />} />
+            <Route path="/public" element={<PublicRoutes />} />
+            <Route path="/collections" element={<NTFCollectionsListPage />} />
         </Routes>
     )
 }
