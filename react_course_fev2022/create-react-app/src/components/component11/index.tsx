4dce17bbf9f8278aec88118c3d2efb748f383c46
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-02-23 18:29:06 +0000

    Continued course

--- react_course_fev2022/create-react-app/src/components/component11/index.tsx
+++ react_course_fev2022/create-react-app/src/components/component11/index.tsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from "../../store";
+import * as example from "../../store/example.duck"
 
 // Functional component
 export const Component11 = () => {
+
+    const dispatch = useDispatch()
 
     const message = useSelector((state: RootState) => {
       return state.example.message;
      })
 
-    return <h1>{message}</h1>;
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const payload = {
+        message: e.target.value
+      }
+
+      dispatch(example.actions.setMessage(payload));
+    }
+
+    return <>
+      <input type="text" onChange={onChange} />
+      <h1>{message}</h1>
+    </>;
 }
