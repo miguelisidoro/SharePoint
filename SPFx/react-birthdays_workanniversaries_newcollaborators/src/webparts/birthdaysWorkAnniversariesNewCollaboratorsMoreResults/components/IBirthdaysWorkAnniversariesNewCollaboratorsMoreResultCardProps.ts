fd6e863acf9734379b52c7255d1ab5c63d9063b5
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-03-02 22:08:10 +0000

    Removed card component

--- SPFx/react-birthdays_workanniversaries_newcollaborators/src/webparts/birthdaysWorkAnniversariesNewCollaboratorsMoreResults/components/IBirthdaysWorkAnniversariesNewCollaboratorsMoreResultCardProps.ts
+++
@@ -1,7 +0,0 @@
-import { DisplayMode } from "@microsoft/sp-core-library";
-import { WebPartContext } from "@microsoft/sp-webpart-base";
-import { PersonaInformation } from "../../../models";
-
-export interface IBirthdaysWorkAnniversariesNewCollaboratorsMoreResultCardProps {
-  user: PersonaInformation[];
-}
--- SPFx/react-birthdays_workanniversaries_newcollaborators/src/webparts/birthdaysWorkAnniversariesNewCollaboratorsMoreResults/components/IBirthdaysWorkAnniversariesNewCollaboratorsMoreResultCardState.ts
+++
@@ -1,5 +0,0 @@
-import { InformationType } from "../../../enums";
-import { PersonaInformation, UserInformation } from "../../../models";
-
-export interface IBirthdaysWorkAnniversariesNewCollaboratorsMoreResultCardState {
-  }
