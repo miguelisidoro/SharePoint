b1d192820f7a0fc882d78e888e6dcf1818399206
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-03-02 17:15:28 +0000

    Changes to package.json and gulpfile.js

--- SPFx/react-birthdays_workanniversaries_newcollaborators/gulpfile.js
+++ SPFx/react-birthdays_workanniversaries_newcollaborators/gulpfile.js
@@ -4,6 +4,27 @@ const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
+// // Uncomment to analyze bundle
+// const bundleAnalyzer = require('webpack-bundle-analyzer');
+// const path = require('path');
+
+// build.configureWebpack.mergeConfig({
+//   additionalConfiguration: (generatedConfiguration) => {
+//     const lastDirName = path.basename(__dirname);
+//     const dropPath = path.join(__dirname, 'temp', 'stats');
+//     generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
+//       openAnalyzer: false,
+//       analyzerMode: 'static',
+//       reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
+//       generateStatsFile: true,
+//       statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
+//       logLevel: 'error'
+//     }));
+
+//     return generatedConfiguration;
+//   }
+// });
+
 var getTasks = build.rig.getTasks;
 build.rig.getTasks = function () {
   var result = getTasks.call(build.rig);
