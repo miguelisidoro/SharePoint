648132bf71fc1618b19ae3da040931cd446216b7
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-03-05 12:32:59 +0000

    Fix

--- SPFx/react-birthdays_workanniversaries_newcollaborators/src/webparts/birthdaysWorkAnniversariesNewCollaboratorsMoreResults/components/index.ts
+++ SPFx/react-birthdays_workanniversaries_newcollaborators/src/webparts/birthdaysWorkAnniversariesNewCollaboratorsMoreResults/components/index.ts
@@ -1,6 +1,3 @@
-export * from './BirthdaysWorkAnniversariesNewCollaboratorsMoreResultCard';
-export * from './IBirthdaysWorkAnniversariesNewCollaboratorsMoreResultCardProps';
-export * from './IBirthdaysWorkAnniversariesNewCollaboratorsMoreResultCardState';
 export * from './BirthdaysWorkAnniversariesNewCollaboratorsMoreResults';
 export * from './IBirthdaysWorkAnniversariesNewCollaboratorsMoreResultsProps';
 export * from './IBirthdaysWorkAnniversariesNewCollaboratorsMoreResultsState';
