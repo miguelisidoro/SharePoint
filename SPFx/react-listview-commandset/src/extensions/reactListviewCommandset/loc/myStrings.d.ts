d53a962612c47b30586004f3c48a272146254af9
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2022-03-18 09:43:43 +0000

    List View command set

---
+++ SPFx/react-listview-commandset/src/extensions/reactListviewCommandset/loc/myStrings.d.ts
@@ -0,0 +1,9 @@
+declare interface IReactListviewCommandsetCommandSetStrings {
+  Command1: string;
+  Command2: string;
+}
+
+declare module 'ReactListviewCommandsetCommandSetStrings' {
+  const strings: IReactListviewCommandsetCommandSetStrings;
+  export = strings;
+}
